3c0c546458cbbef934e4139f94077ed67b3cd22c
Conan Theobald <github@conans.co.uk>
2023-02-13 10:45:35 +0000

    chore: add LaunchCardList to Home page, test

---
+++ tests/index.spec.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+
+import Home from '@/pages/index';
+import mockData from '@/pages/api/launches/mock.json';
+import { launchCardPropsFromApiDoc } from '@/schemas/adapters/launch-card-props-from-doc';
+
+const cards = (mockData.docs ?? []).map(launchCardPropsFromApiDoc);
+
+describe('Home', () => {
+  it('renders the cards with mock-data', () => {
+    render(<Home cards={cards} />);
+
+    const heading = screen.getByRole('heading', { name: /FalconSat/i });
+    expect(heading).toBeInTheDocument();
+  });
+});
