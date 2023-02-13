a4ac7ba6af130c5cdfd0b145b84b418abefa1f1d
Conan Theobald <github@conans.co.uk>
2023-02-13 10:13:46 +0000

    build: install msw, configure

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['images2.imgbox.com'],
   },
-}
+  webpack: (config) => {
+    /**
+     * Top-level await required to ensure Next.js getServersideProps
+     * doesn't get run before MSW is ready.
+     *
+     * Also, MSW will not work with Node 18. Node 16 used in this repo.
+     *
+     * @see https://github.com/mswjs/msw/issues/1340
+     */
+    config.experiments.topLevelAwait = true;
+    return config;
+  },
+};
+
+// eslint-disable-next-line no-undef
+module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,17 @@
 import '../styles/globals.css';
 
+if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
+  /**
+   * Top-level await required to ensure Next.js getServersideProps
+   * doesn't get run before MSW is ready.
+   *
+   * Also, MSW will not work with Node 18. Node 16 used in this repo.
+   *
+   * @see https://github.com/mswjs/msw/issues/1340
+   */
+  await import('../mocks');
+}
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
