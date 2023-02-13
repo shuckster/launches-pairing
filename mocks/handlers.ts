36e0d62258ee830f783f4bc9238e99b734016e4a
Conan Theobald <github@conans.co.uk>
2023-02-13 10:40:27 +0000

    feat: add spacex launches to msw handlers

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,4 +1,19 @@
 import { rest } from 'msw';
+import mockData from '@/pages/api/launches/mock.json';
+
+import type {
+  TPartialLaunchesDocumentSchema,
+  TSpaceXApiQueryResponse,
+} from '@/schemas/api/spacex.types';
+
+type TLaunchesApiResponse =
+  TSpaceXApiQueryResponse<TPartialLaunchesDocumentSchema>;
 
 export const handlers = [
+  rest.post(
+    'https://api.spacexdata.com/v5/launches/query',
+    (_req, res, ctx) => {
+      return res(ctx.json<TLaunchesApiResponse>(mockData));
+    }
+  ),
 ];
